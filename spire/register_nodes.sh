415b8e3937e0865bfbed68b79f6696a71242c765
Sabree Blackmon <sabree@lovewellgroup.com>
2018-10-09 18:29:30 -0500

    Fluentd over Envoy

--- spire/register_nodes.sh
+++ spire/register_nodes.sh
@@ -6,9 +6,13 @@
 # Create policy
 docker-compose exec spire-server sh -c "cd /opt/spire && ./spire-server entry create -parentID spiffe://example.org/simple-secrets -spiffeID spiffe://example.org/simple-secrets1 -selector unix:uid:0 -ttl 120"
 docker-compose exec spire-server sh -c "cd /opt/spire && ./spire-server entry create -parentID spiffe://example.org/prometheus -spiffeID spiffe://example.org/prometheus-proxy -selector unix:uid:0 -ttl 120"
+docker-compose exec spire-server sh -c "cd /opt/spire && ./spire-server entry create -parentID spiffe://example.org/fluentd -spiffeID spiffe://example.org/fluentd-proxy -selector unix:uid:0 -ttl 120"
 
 docker-compose exec -d simple-secrets sh -c "cd /opt/spire && ./spire-agent run -joinToken $(docker-compose exec spire-server sh -c 'cd /opt/spire && ./spire-server token generate -spiffeID spiffe://example.org/simple-secrets' | sed 's/Token: //' | sed "s/$(printf '\r')//")"
 docker-compose exec -d simple-secrets sh -c "cd /opt/spiffe-helper && ./sidecar"
 
 docker-compose exec -d prometheus-proxy sh -c "cd /opt/spire && ./spire-agent run -joinToken $(docker-compose exec spire-server sh -c 'cd /opt/spire && ./spire-server token generate -spiffeID spiffe://example.org/prometheus' | sed 's/Token: //' | sed "s/$(printf '\r')//")"
 docker-compose exec -d prometheus-proxy sh -c "cd /opt/spiffe-helper && ./sidecar"
+
+docker-compose exec -d fluentd-proxy sh -c "cd /opt/spire && ./spire-agent run -joinToken $(docker-compose exec spire-server sh -c 'cd /opt/spire && ./spire-server token generate -spiffeID spiffe://example.org/fluentd' | sed 's/Token: //' | sed "s/$(printf '\r')//")"
+docker-compose exec -d fluentd-proxy sh -c "cd /opt/spiffe-helper && ./sidecar"
